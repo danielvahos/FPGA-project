a5b03d5ca24ec439c7ae7c4a44725c61135f0f75
Daniel <daniel.vahosmendoza@telecom-paris.fr>
2022-02-13 18:51:49 +0100

    wshb_ifm.clk, wshb_ifm.rst correction

--- controleur_video/SoCFPGA/src/mire.sv
+++ controleur_video/SoCFPGA/src/mire.sv
@@ -9,7 +9,7 @@ logic [$clog2(VDISP)-1:0] count_line;
 
 
 //For the counters (pixels and lines) From top-down
-always_ff @(posedge pixel_clk or posedge pixel_rst)
+always_ff @(posedge wshb_ifm.clk or posedge wshb_ifm.rst)
 begin
     if (pixel_rst)
     begin
